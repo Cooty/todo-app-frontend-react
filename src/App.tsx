f722db965b189f7c243d2d39a45664be384e3230
Tamas Kuti <cooty13@gmail.com>
2022-06-20 17:03:20 +0200

    feat: display number of open todos

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import AddToDo from "./components/AddToDo/AddToDo";
 import ToDoContext from "./store/context";
 import AppWrapper from "./components/AppWrapper";
 import Header from "./components/Header";
+import countOpenToDos from "./utils/count-open-todos";
 
 const App: React.FC = () => {
   const ctx = useContext(ToDoContext);
+  const headerText =
+    countOpenToDos(ctx.todos) === 0
+      ? "Everything's done!"
+      : `Things to do (${countOpenToDos(ctx.todos)}):`;
 
   return (
     <AppWrapper>
       <AddToDo inputId="todo-input" placeholder="Start typing to add a ToDo" />
       {ctx.todos.length ? (
         <React.Fragment>
-          <Header>Things to do:</Header>
+          <Header>{headerText}</Header>
           <ToDoList
             items={ctx.todos}
             onDeleteTodo={ctx.delete}
